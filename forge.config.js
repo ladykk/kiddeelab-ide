a93757254498b7acbf8aa20f25524f33267ed262
Rattapon Apiratjit <krtp.apirat@gmail.com>
2023-01-03 13:40:00 +0700

    update deploy option

--- forge.config.js
+++ forge.config.js
@@ -12,18 +12,22 @@ module.exports = {
     {
       name: "@electron-forge/maker-squirrel",
       config: {
+        name: "th.co.kiddeelab.ide",
+        title: "KiddeeLab IDE",
+        iconUrl:
+          "https://firebasestorage.googleapis.com/v0/b/kiddeelab.appspot.com/o/icon.ico?alt=media&token=0fd158d8-f318-4d6f-a4b6-1e58000db438",
         setupIcon: "src/assets/icon/icon.ico",
       },
     },
-    {
-      name: "@electron-forge/maker-dmg",
-      config: {
-        format: "ULFO",
-        icon: "src/assets/icon/icon.icns",
-      },
-    },
-    {
-      name: "@electron-forge/maker-zip",
-    },
+    // {
+    //   name: "@electron-forge/maker-dmg",
+    //   config: {
+    //     format: "ULFO",
+    //     icon: "src/assets/icon/icon.icns",
+    //   },
+    // },
+    // {
+    //   name: "@electron-forge/maker-zip",
+    // },
   ],
 };
