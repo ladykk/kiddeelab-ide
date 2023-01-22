6efab69ad429525f464e493075bbca958ac8a576
Rattapon Apiratjit <krtp.apirat@gmail.com>
2023-01-22 20:34:56 +0700

    add components. (dht, ultrsonic)

---
+++ src/devices/components/dht/index.ts
@@ -0,0 +1,140 @@
+import { defineBlocksWithJsonArray, Block } from "blockly";
+import ArduinoGenerator from "../../../blocks/arduino";
+import { ORDER } from "../../../blocks/arduino/generator";
+import { ComponentDetail } from "../../../types/component";
+import blocks from "./dht.json";
+
+defineBlocksWithJsonArray(blocks);
+
+ArduinoGenerator["component_dht_begin"] = function (block: Block) {
+  const name: string = ArduinoGenerator.valueToCode(
+    block,
+    "name",
+    ORDER.ATOMIC
+  );
+  return `${name}.begin();\n`;
+};
+
+ArduinoGenerator["component_dht_read_humidity"] = function (block: Block) {
+  const name: string = ArduinoGenerator.valueToCode(
+    block,
+    "name",
+    ORDER.ATOMIC
+  );
+
+  return [`${name}.readHumidity()`, ORDER.ATOMIC];
+};
+
+ArduinoGenerator["component_dht_read_temperature"] = function (block: Block) {
+  const unit: string = block.getFieldValue("unit");
+  const name: string = ArduinoGenerator.valueToCode(
+    block,
+    "name",
+    ORDER.ATOMIC
+  );
+
+  return [
+    `${name}.readTemperature(${unit === "F" ? "true" : ""})`,
+    ORDER.ATOMIC,
+  ];
+};
+
+ArduinoGenerator["component_dht_read_heat_index"] = function (block: Block) {
+  const name: string = ArduinoGenerator.valueToCode(
+    block,
+    "name",
+    ORDER.ATOMIC
+  );
+
+  return [
+    `${name}.computeHeatIndex(${name}.readTemperature(), ${name}.readHumidity(), false)`,
+    ORDER.ATOMIC,
+  ];
+};
+
+const DHT: ComponentDetail = {
+  id: "dht",
+  name: "Temperature & Humidity",
+  pins: ["PIN"],
+  supported_devices: "*",
+  blocks(component, deviceId) {
+    return [
+      {
+        kind: "label",
+        text: component.name,
+      },
+      {
+        kind: "block",
+        type: "component_dht_declare",
+        inputs: {
+          name: {
+            block: {
+              type: "pin_define",
+              fields: { pin: component.name },
+            },
+          },
+          pin: {
+            block: {
+              type: "pin_define",
+              fields: { pin: component.pins[0].pin },
+            },
+          },
+        },
+      },
+      {
+        kind: "block",
+        type: "component_dht_begin",
+        inputs: {
+          name: {
+            block: {
+              type: "pin_define",
+              fields: { pin: component.name },
+            },
+          },
+        },
+      },
+      {
+        kind: "block",
+        type: "component_dht_read_humidity",
+        inputs: {
+          name: {
+            block: {
+              type: "pin_define",
+              fields: { pin: component.name },
+            },
+          },
+        },
+      },
+      {
+        kind: "block",
+        type: "component_dht_read_temperature",
+        inputs: {
+          name: {
+            block: {
+              type: "pin_define",
+              fields: { pin: component.name },
+            },
+          },
+        },
+      },
+      {
+        kind: "block",
+        type: "component_dht_read_heat_index",
+        inputs: {
+          name: {
+            block: {
+              type: "pin_define",
+              fields: { pin: component.name },
+            },
+          },
+        },
+      },
+    ];
+  },
+  module_declare: (component, deviceId) => ["include <DHT.h>"],
+  variable_declare: (compnent, deviceId) => [
+    `DHT ${compnent.name}(${compnent.pins[0].pin}, DHT22)`,
+  ],
+};
+
+export default DHT;
---
+++ src/devices/components/ultrasonic/index.ts
@@ -0,0 +1,64 @@
+import { Block, defineBlocksWithJsonArray } from "blockly";
+import ArduinoGenerator from "../../../blocks/arduino";
+import { ORDER } from "../../../blocks/arduino/generator";
+import { ComponentDetail } from "../../../types/component";
+import blocks from "./ultrasonic.json";
+
+defineBlocksWithJsonArray(blocks);
+
+ArduinoGenerator["component_ultrasonic_read"] = function (block: Block) {
+  const trig: string = ArduinoGenerator.valueToCode(
+    block,
+    "trig",
+    ORDER.ATOMIC
+  );
+  const echo: string = ArduinoGenerator.valueToCode(
+    block,
+    "echo",
+    ORDER.ATOMIC
+  );
+
+  return [`readUltrasonic(${trig}, ${echo})`, ORDER.ATOMIC];
+};
+
+const Ultrasonic: ComponentDetail = {
+  id: "ultrasonic",
+  name: "Ultrasonic",
+  pins: ["TRIG", "ECHO"],
+  supported_devices: "*",
+  blocks: (component, deviceId) => {
+    return [
+      {
+        kind: "label",
+        text: component.name,
+      },
+      {
+        kind: "block",
+        type: "component_ultrasonic_read",
+        inputs: {
+          trig: {
+            block: {
+              type: "pin_define",
+              fields: {
+                pin: component.pins[0].pin,
+              },
+            },
+          },
+          echo: {
+            block: {
+              type: "pin_define",
+              fields: {
+                pin: component.pins[1].pin,
+              },
+            },
+          },
+        },
+      },
+    ];
+  },
+  function_declare: (component, deviceId) => [
+    "double readUltrasonic(int trigPin, int echoPin) {\n  pinMode(trigPin, OUTPUT);\n  digitalWrite(trigPin, LOW);\n  delayMicroseconds(2);\n  digitalWrite(trigPin, HIGH);\n  delayMicroseconds(5);\n  digitalWrite(trigPin, LOW);\n  pinMode(echoPin, INPUT);\n  return pulseIn(echoPin, HIGH) / 29.0 / 2.0;\n}",
+  ],
+};
+
+export default Ultrasonic;
