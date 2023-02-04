fce136ec7731f8cd09c250137966c5b20f78393d
Rattapon Apiratjit <krtp.apirat@gmail.com>
2023-02-04 15:33:21 +0700

    add website link

--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -15,11 +15,13 @@ function MenuBar() {
       className={`w-full h-[56px] bg-blue-500 py-2 px-3 flex gap-2 items-center justify-between`}
     >
       <div className="flex items-center h-full gap-2 flex-1">
-        <img
-          src={LOGO}
-          alt="KiddeeLab"
-          className="bg-white p-[2px] w-[43px] h-[40px] aspect-square rounded"
-        />
+        <a href="https://www.kiddeelab.co.th/" target="_blank">
+          <img
+            src={LOGO}
+            alt="KiddeeLab"
+            className="bg-white p-[2px] w-[43px] h-[40px] aspect-square rounded"
+          />
+        </a>
 
         <Dropdown label={<div className="flex gap-2">Files</div>} size="sm">
           <NewProject />
